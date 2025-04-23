429820927a5ba7384ea7db416da2c618df21d5e9
Alvin K <alvin.kita@outlook.fr>
2025-04-23 10:13:51 +0200

    Responsive background

--- App.js
+++ App.js
@@ -118,7 +118,8 @@ export default function App() {
 const styles = StyleSheet.create({
   imageBackground: {
     flex: 1,
-    resizeMode: 'cover',
+    width: '100%',
+    height: '100%',
   },
   container: {
     flex: 1,
